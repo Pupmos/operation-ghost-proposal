45704333e9a022f31e57d7ea9c1245f396a885b9
Sirius Taikun <siriustaikun@protonmail.com>
2023-02-23 19:01:51 -0700

    add changelog

--- upload.sh
+++
@@ -1,5 +0,0 @@
-junod tx wasm store ./artifacts/pfc_steak_hub.wasm \
-  --node https://juno-rpc.polkachu.com:443 \
-  --chain-id juno-1 --from steak-admin \
-  --gas-prices 1.1ujuno --gas 5000000 \
-  --gas-adjustment 1.5 -y -b block
