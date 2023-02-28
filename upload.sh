abba4eaa36d7ae865f3074afd5a63894f54b75a4
Sirius Taikun <siriustaikun@protonmail.com>
2023-02-27 21:21:29 -0700

    added mirrorbox

--- upload.sh
+++ upload.sh
@@ -1,5 +1,8 @@
-junod tx wasm store ./artifacts/smokescreen.wasm \
+junod tx wasm store ./artifacts/trapdoor.wasm \
   --node https://juno-rpc.polkachu.com:443 \
-  --chain-id juno-1 --from red-dao-lpx-psyop \
-  --gas-prices 1.1ujuno --gas 5000000 \
+  --chain-id juno-1 --from flix-pupmos-burner \
+  --gas-prices 1.5ujuno --gas 5000000 \
   --gas-adjustment 1.5 -y -b block
+
+  # 899.9K $TOKEN
+
